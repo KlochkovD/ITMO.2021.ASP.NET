23057bf970990344531ba68982f7866655f149d5
nb <disha07@mail.ru>
2021-09-30 20:24:56 +0300

    -

--- WebMVCR1/Controllers/MyController.cs
+++ WebMVCR1/Controllers/MyController.cs
@@ -11,10 +11,35 @@ namespace WebMVCR1.Controllers
     {
         public string Start(string hel)
         {
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+
+            //string res = ExeEnum();
+            string res = ExeStruct();
+            return res;
 
         }
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            string res1 = String.Format("Тип банковского счета {0}",
+            goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}",
+            platinumAccount);
+            string res = res1 + "<p>" + res2;
+            return res;
 
+
+        }
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+            string str = $"Номер счета {goldBankAccount.accNo}, баланс {goldBankAccount.accBal}, тип {goldBankAccount.accNo}";
+            return str;
+        }
     }
 }
---
+++ WebMVCR1/Models/StudyCsharp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCR1.Models
+{
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    public class StudyCsharp
+    {
+
+    }
+    public struct BankAccount
+
+   {
+    public long accNo;
+    public decimal accBal;
+    public AccountType accType;
+    public override string ToString()
+    {
+        string res = String.Format("Номер счета {0}", accNo);
+        return res;
+    }
+}
+}
+
